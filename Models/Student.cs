011cd63e804e95cb1811dce1906e55e32032b717
ADMIN <ADMIN@KAPIL>
2023-08-09 13:00:39 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactAspCrud.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+        public string stname { get; set; }
+        public string course { get; set; }
+
+    }
+}
---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source =.; Initial Catalog=kapilDb; TrustServerCertificate=True; Integrated Security=True");   
+        }
+    }
+}
